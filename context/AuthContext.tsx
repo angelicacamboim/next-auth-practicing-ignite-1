7fae694e6a8c4b90f5bc9c2a3c9aea95f3839b88
rodrigodevelop-tech <digoalves12@hotmail.com>
2022-01-24 23:43:27 -0300

    :rockeat:

---
+++ context/AuthContext.tsx
@@ -0,0 +1,94 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from 'nookies';
+import Router from 'next/router';
+
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials : SignInCredentials) : Promise<void>;
+  user : User | undefined;
+  isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+
+export function AuthProvider({ children } : AuthProviderProps){
+  const [user, setUser] = useState<User>();
+
+  const isAuthenticated = !!user;
+
+  useEffect(()=> {
+    const { 'nextAuth.token': token } = parseCookies();
+
+    if(token){
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data;
+
+        setUser({
+          email, 
+          permissions, 
+          roles
+        });
+
+      });
+    }
+  },[]);
+
+  async function signIn({email,password } : SignInCredentials){
+    try{
+      const response = await api.post('sessions', {
+        email,
+        password,
+      });
+  
+      const { token, refreshToken ,permissions, roles } = response.data;
+
+      setCookie(undefined, 'nextAuth.token', token, {
+        maxAge: 60 * 60 * 25 * 30, // 30 days
+        path: '/'
+      });
+      setCookie(undefined, 'nextAuth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 25 * 30, // 30 days
+        path: '/'
+      });
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      Router.push('/dashboard');
+
+    }catch(err){
+      console.log(err);
+    }
+
+  }
+
+  return(
+    <AuthContext.Provider value={{
+      signIn,
+      user,
+      isAuthenticated,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
