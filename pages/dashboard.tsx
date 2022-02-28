a716d82a41f32a6a3327fdc5bad649d929e421ca
Angelica Bayer <angelicacamboim@gmail.com>
2022-02-27 22:07:04 -0300

    adjusted

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,37 +1,38 @@
-import { useContext, useEffect } from "react"
-import { Can } from "../components/Can";
-import { AuthContext } from "../context/AuthContext"
-import { setupAPIClient } from "../services/api";
-import { api } from "../services/apiClient";
-import { withSSRAuth } from "../utils/withSSRAuth";
+import { useContext, useEffect } from 'react'
+import { Can } from '../components/Can'
+import { AuthContext } from '../context/AuthContext'
+import { setupAPIClient } from '../services/api'
+import { api } from '../services/apiClient'
+import { withSSRAuth } from '../utils/withSSRAuth'
 
-export default function Dashboard(){
-  const { user, signOut }= useContext(AuthContext);
+export default function Dashboard() {
+	const { user, signOut } = useContext(AuthContext)
 
-  useEffect(()=>{
-    api.get('/me')
-    .then(response => console.log(response))
-    .catch(err => console.log(err));
-  },[]);
+	useEffect(() => {
+		api
+			.get('/me')
+			.then((response) => console.log(response))
+			.catch((err) => console.log(err))
+	}, [])
 
-  return(
-    <>
-      <h1>Dashboard: {user?.email}</h1>
+	return (
+		<>
+			<h1>Dashboard: {user?.email}</h1>
 
-      <button onClick={signOut}>Sign Out</button>
-    
-      <Can permissions={['metrics.list']}>
-        <div>Métricas</div>
-      </Can>
-    </>
-  )
+			<button onClick={signOut}>Sign Out</button>
+
+			<Can permissions={['metrics.list']}>
+				<div>Métricas</div>
+			</Can>
+		</>
+	)
 }
 
 export const getServerSideProps = withSSRAuth(async (context) => {
-  const apiClient = setupAPIClient(context);
-  const response = await apiClient.get('/me');
+	const apiClient = setupAPIClient(context)
+	const response = await apiClient.get('/me')
 
-  return {
-    props: {}
-  }
+	return {
+		props: {}
+	}
 })
